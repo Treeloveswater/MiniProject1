657acb1df3b9051f88653bc53ac8015291b7c670
Treeloveswater <1213677015@qq.com>
2022-07-20 20:24:41 +0800

    0720.2

--- MiniProject/Assets/Scripts/PlayerControler.cs
+++ MiniProject/Assets/Scripts/PlayerControler.cs
@@ -9,21 +9,51 @@ public class PlayerControler : MonoBehaviour
     public GameObject role;
     [SerializeField] public float speed = 5.0f;
 
-    private Rigidbody2D _rigidbody2D = null;
+    public float jumpspeed = 15f;
+    //public float gravity = 1;
+    //public float fallMultiplier = 5f;
 
+    Camera https://www.youtube.com/watch?v=USLp-4iwNnQ;
+
+    public LayerMask groundLayer;
+    public GameObject groundpos;
+    private Rigidbody2D _rigidbody2D = null;
+    private float facedirection;
     private Vector2 inputmove = Vector2.zero;
+
+    private bool dojump;
+
     void Start()
     {
+
         _rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
+
+    public bool OnGround = false;
+    public float groundLength = 0.6f;
 
     // Update is called once per frame
     void Update()
     {
+        OnGround = Physics2D.Raycast(groundpos.transform.position, Vector2.down, groundLength, groundLayer);
+
         inputmove = new Vector2(Input.GetAxis("Horizontal"), 0f);
+        float facedirection = Input.GetAxisRaw("Horizontal");
         inputmove.Normalize();
+        if(Input.GetButtonDown("Jump") && OnGround)
+        {
+            dojump = true;
+        }
 
         if(inputmove == Vector2.zero || _rigidbody2D == null) return;
+
+
+        
+    }
+    void Jump()
+    {
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0);
+        _rigidbody2D.AddForce(Vector2.up * jumpspeed, ForceMode2D.Impulse);
 
     }
 
@@ -32,6 +62,21 @@ public class PlayerControler : MonoBehaviour
         if (inputmove == Vector2.zero || _rigidbody2D == null) return;
         //_rigidbody2D.velocity = inputmove * speed;
         _rigidbody2D.position = _rigidbody2D.position + speed * inputmove * Time.deltaTime;
+        if (facedirection != 0)
+        {
+            _rigidbody2D.transform.localScale = new Vector3(facedirection, 1, 1);
+        }
+        if (dojump)
+        {
+            dojump = false;
+            Jump();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundpos.transform.position, groundpos.transform.position + Vector3.down * groundLength);
     }
 }
     
